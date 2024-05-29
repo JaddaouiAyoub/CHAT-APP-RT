591cd4e2725a676a3053c233b37b6da83e8ef4db
JaddaouiAyoub <139131249+JaddaouiAyoub@users.noreply.github.com>
2024-05-29 19:42:46 +0200

    Modification css login et signup

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -12,50 +12,42 @@ const Login = () => {
         e.preventDefault();
         await login(username,password)
     }
-  return (
-    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-        <div className="w-full p-6 rounded-1g shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-            <h1 className="text-3xl font-semibold text-center text-gray-300">
-                Login
-                <span className="text-blue-500"> ChatApp</span>
-            </h1>
+    return (
+        <div className="font-sans text-lg">
+            <div className='flex flex-col items-center justify-center min-w-custom mx-auto'>
+            
+                <div className='w-full p-8 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+                    <h1 className='text-3x1 font-bold text-center text-gray-300 ' style={{ fontSize: '1.5rem' }}>Login <span className='text-blue-800'> ChatApp</span>
+                    </h1>
+                    <form onSubmit= {handleSubmit}>
+                        <div>
+                            <label className='label p-2'>
+                                <span className='text-base label-text' >Username</span>
+                            </label>
+                            <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10 bg-gray-200"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)} />
+                        </div>
+                        <div>
+                            <label className='label'>
+                                <span className='text-base label-text'>Password</span>
+                            </label>
+                            <input type="password" placeholder="Enter password" className="w-full input input-bordered h-10 bg-gray-200"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} />
+                        </div>
 
-            <form onSubmit={handleSubmit}>
-                <div>
-                   <label className="label p-2">
-                      <span className="text-base label-text text-gray-100">Username</span> 
-                   </label> 
-                   <input type="text" placeholder="Enter Username" className="w-full input input-bordered h-10"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                   />
+                        <Link to='/signup' className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">Don't have an account?</Link>
+                        <div>
+                            <button className='btn btn-block btn-sm mt-2 bg-gray-300' disabled={loading}>
+                                {loading ? <span className='loading loading-spinner'></span> : "Login"}
+                            </button>
+                        </div>
+                    </form>
                 </div>
-
-                <div>
-                   <label className="label p-2">
-                      <span className="text-base label-text text-gray-100">Password</span> 
-                   </label> 
-                   <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10" 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                   />
-                </div>
-
-                <Link to="/signup" className="text-sm hover:inderline hover:text-blue-600 mt-2 inline-block text-gray-100" >
-                    {"Don't"} have account?
-                </Link>
-
-                <div>
-                    <button className="btn btn-block btn-sm mt-2 text-gray-100" 
-                        disabled={loading}
-                    >
-                        {loading ? <span className="loading loading-spinner"></span> : "Login"}
-                    </button>
-                </div>
-            </form>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default Login
